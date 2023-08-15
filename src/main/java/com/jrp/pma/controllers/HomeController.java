6910e06385c0ac3b331c926ca034233b36f4feb7
saketchirania <saketchirania@gmail.com>
2023-08-15 23:04:00 +0530

    Working till lecture 16

---
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -0,0 +1,37 @@
+package com.jrp.pma.controllers;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Employee;
+import com.jrp.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+
+    @GetMapping("/")
+    public String displayHome(Model model)
+    {
+        // we are querying the database for projects
+        List<Project> project = projectRepository.findAll();
+        model.addAttribute("projects",project );
+
+        // we are querying the database for employess
+        List<Employee> employees = employeeRepository.findAll();
+        model.addAttribute("employees",employees);
+        return "home.html";
+    }
+
+
+}
