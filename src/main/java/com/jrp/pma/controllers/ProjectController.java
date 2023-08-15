567cb0b778efb99b63a70a518fb593f57915bff3
saketchirania <saketchirania@gmail.com>
2023-08-15 22:22:03 +0530

    Working till lecture 14

---
+++ src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -0,0 +1,36 @@
+package com.jrp.pma.controllers;
+
+import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository proReop;
+
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model){
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+        return "new-project";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Project project, Model model){
+        // use a redirect to prevent duplicate submission
+        proReop.save(project);
+        return "redirect:/projects/new";
+    }
+
+}
---
+++ src/main/java/com/jrp/pma/entities/Project.java
@@ -0,0 +1,84 @@
+package com.jrp.pma.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class  Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long projectId;
+    private String name;
+    private String description;
+    private String stage; //inprogess, complted, etc
+
+//
+//    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH,CascadeType.PERSIST},
+//            fetch = FetchType.LAZY)
+//    @JoinTable(name = "project_employee",
+//            joinColumns =@JoinColumn (name = "project_id"),
+//            inverseJoinColumns = @JoinColumn(name = "employee_id"))
+//    private List<Employee> employees;
+
+    public Project(){
+    }
+
+    public Project(String name, String stage, String description) {
+        super();
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+
+    }
+
+//    public List<Employee> getEmployees() {
+//        return employees;
+//    }
+//
+//    public void setEmployees(List<Employee> employees) {
+//        this.employees = employees;
+//    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+//    public void addEmployee(Employee emp) {
+//        if (employees==null)
+//        {
+//            employees =  new ArrayList<>();
+//        }
+//        employees.add(emp);
+//    }
+}
