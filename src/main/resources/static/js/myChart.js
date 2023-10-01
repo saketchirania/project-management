5cccf63f1dc81b8e68209fb032abcd7debe857aa
saketchirania <saketchirania@gmail.com>
2023-10-01 18:45:52 +0530

    Working till lecture 24.

---
+++ src/main/resources/static/js/myChart.js
@@ -0,0 +1,35 @@
+var myPieChart = new Chart(ctx,{
+    type: 'pie',
+    data: data,
+    options: options
+});
+
+//// For a pie chart
+//new Chart(document.getElementById("myPieChart"), {
+//    type: 'pie',
+//    // The data for our dataset
+//    data: {
+//        labels: labelData,
+//        datasets: [{
+//            label: 'My First dataset',
+//            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+//            data: numericData
+//        }]
+//    },
+//
+//    // Configuration options go here
+//    options: {
+//    	title: {
+//    		display: true,
+//    		text: 'Project Statuses'
+//    	}
+//
+//    }
+//});
+//
+//// "[{"value": 1, "label": "COMPLETED"},{"value": 2, "label": "INPROGRESS"},{"value": 1, "label": "NOTSTARTED"}]"
+//function decodeHtml(html){
+//	var txt = document.createElement("textarea");
+//	txt.innerHTML = html;
+//	return txt.value;
+//}
